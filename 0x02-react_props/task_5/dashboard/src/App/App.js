55a7cf5a4f0e20b6da42921ef2615cde882dbaed
Sally-N <snzungula@gmail.com>
2023-08-31 19:06:37 +0000

    Shapes, Loops, and Keys

--- 0x02-react_props/task_5/dashboard/src/App/App.js
+++ 0x02-react_props/task_5/dashboard/src/App/App.js
@@ -6,14 +6,27 @@ import Footer from '../Footer/Footer';
 import Notifications from '../Notifications/Notifications';
 import CourseList from '..CourseList/CourseList';
 import PropTypes from 'prop-types';
+import { getLatestNotification } from '../utils/utils';
 
 function App({isLoggedIn}) {
+  const listCourses = [
+    {id: 1, name: 'ES6', credit: 60},
+    {id: 2, name: 'Webpack', credit: 20},
+    {id: 3, name: 'React', credit:40}
+  ]
+
+  const listNotifications = [
+    {id: 1, type: 'default', value: 'New course available'},
+    {id: 2, type: 'urgent', value: 'New resume available'},
+    {id: 3, type: 'urgent', html: getLatestNotification()}
+  ]
+
   return (
     <>
-      <Notifications />
+      <Notifications listNotifications={listNotifications}/>
       <div className="App">
         <Header />
-        {isLoggedIn ? <CourseList /> : <Login />}
+        {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
         <Footer />
       </div>
     </>
