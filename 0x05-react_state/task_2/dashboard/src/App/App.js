543e881b294fb103b33ca878ff64dc2dbc88d4ce
Sally-N <snzungula@gmail.com>
2023-09-12 22:34:44 +0300

    use context

--- 0x05-react_state/task_2/dashboard/src/App/App.js
+++ 0x05-react_state/task_2/dashboard/src/App/App.js
@@ -4,12 +4,11 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
 import CourseList from '../CourseList/CourseList';
-import PropTypes from 'prop-types';
 import Notifications from '../Notifications/Notifications';
 import { getLatestNotification } from '../utils/utils';
 import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
 import { StyleSheet, css } from 'aphrodite';
-import { logOut, user } from './AppContext';
+import { user, AppContext } from './AppContext';
 
 const styles = StyleSheet.create({
 	App: {
@@ -38,13 +37,15 @@ class App extends React.Component {
 		this.state = {
 			displayDrawer: false,
 			user: user,
-			logOut: logOut,
+			logOut: this.logOut,
 		};
-	
+
 
 		this.handleKeyPress = this.handleKeyPress.bind(this);
 		this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
 		this.handleHideDrawer = this.handleHideDrawer.bind(this);
+		this.logIn = this.logIn.bind(this);
+		this.logOut = this.logOut.bind(this);
 
 	}
 
@@ -63,7 +64,7 @@ class App extends React.Component {
 	handleKeyPress(e) {
 		if (e.ctrlKey && e.key === 'h') {
 			alert('Logging you out');
-			this.props.logOut();
+			this.logOut();
 		}
 	}
 
@@ -83,49 +84,60 @@ class App extends React.Component {
 		this.setState({ displayDrawer: false });
 	}
 
+	logIn(email, password) {
+		this.setState({
+			user: {
+				email, password, isLoggedIn: true
+			},
+		});
+	}
+
+	logOut() {
+		this.setState({
+			user: user,
+		})
+	}
+
 	render() {
 		return (
-			<React.Fragment>
-				<div className={css(styles.App)}>
-					<div className={css(styles['heading-section'])}>
-						<Notifications
-							listNotifications={this.listNotifications}
-							displayDrawer={this.state.displayDrawer}
-							handleDisplayDrawer={this.handleDisplayDrawer} handleHideDrawer={this.handleHideDrawer} />
-						<Header />
+			<AppContext.Provider value={{
+				user: this.state.user,
+				logOut: this.state.logOut,
+			}}>
+				<React.Fragment>
+					<div className={css(styles.App)}>
+						<div className={css(styles['heading-section'])}>
+							<Notifications
+								listNotifications={this.listNotifications}
+								displayDrawer={this.state.displayDrawer}
+								handleDisplayDrawer={this.handleDisplayDrawer}
+								handleHideDrawer={this.handleHideDrawer} />
+							<Header />
+						</div>
+						{user.isLoggedIn ? (
+							<BodySectionWithMarginBottom title='Course list'>
+								<CourseList listCourses={this.listCourses} />
+							</BodySectionWithMarginBottom>
+						) : (
+							<BodySectionWithMarginBottom title='Log in to continue'>
+								<Login login={this.logIn} />
+							</BodySectionWithMarginBottom>
+						)}
+						<BodySection title='News from the school'>
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Perspiciatis at tempora odio, necessitatibus repudiandae
+								reiciendis cum nemo sed asperiores ut molestiae eaque aliquam illo
+								ipsa iste vero dolor voluptates.
+							</p>
+						</BodySection>
+						<Footer className={css(styles['App-footer'])} />
 					</div>
-					{this.props.isLoggedIn ? (
-						<BodySectionWithMarginBottom title='Course list'>
-							<CourseList listCourses={this.listCourses} />
-						</BodySectionWithMarginBottom>
-					) : (
-						<BodySectionWithMarginBottom title='Log in to continue'>
-							<Login />
-						</BodySectionWithMarginBottom>
-					)}
-					<BodySection title='News from the school'>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit.
-							Perspiciatis at tempora odio, necessitatibus repudiandae
-							reiciendis cum nemo sed asperiores ut molestiae eaque aliquam illo
-							ipsa iste vero dolor voluptates.
-						</p>
-					</BodySection>
-					<Footer className={css(styles['App-footer'])} />
-				</div>
-			</React.Fragment>
+				</React.Fragment>
+			</AppContext.Provider>
 		);
 	}
 }
 
-App.defaultProps = {
-	isLoggedIn: false,
-	logOut: () => { },
-};
-
-App.propTypes = {
-	isLoggedIn: PropTypes.bool,
-	logOut: PropTypes.func,
-};
 
 export default App;
