67b5002316d681179e7b332d925c34f2d872bd2f
Sally-N <snzungula@gmail.com>
2023-09-12 21:42:26 +0300

    login tests

--- 0x05-react_state/task_1/dashboard/src/Login/Login.test.js
+++ 0x05-react_state/task_1/dashboard/src/Login/Login.test.js
@@ -10,14 +10,27 @@ afterEach(() => {
 	StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
 });
 
-describe('Header', () => {
+describe('Login', () => {
 	it('should render without crashing', () => {
 		const wrapper = shallow(<Login />);
 		expect(wrapper.exists()).toEqual(true);
 	});
+
 	it('should have 2 input tags and 2 label tags', () => {
 		const wrapper = shallow(<Login />);
 		expect(wrapper.find('label')).toHaveLength(2);
 		expect(wrapper.find('input')).toHaveLength(2);
+	});
+
+	it("verify that the submit button is disabled by default", () => {
+		const wrapper = shallow(<Login />)
+		expect(wrapper.find("input[type='submit']").props().disabled).toEqual(true);
+	});
+
+	it("verify that after changing the value of the two inputs, the button is enabled", () => {
+		const wrapper = shallow(<Login />)
+		wrapper.find({ name: "email" }).simulate('change', { target: { value: 't' } });
+		wrapper.find({ name: "password" }).simulate('change', { target: { value: 't' } });
+		expect(wrapper.find("input[type='submit']").props().disabled).toEqual(false);
 	});
 });
