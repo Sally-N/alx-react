e41037f20831523901e8ddfb3d479d27528ac8d2
Sally-N <snzungula@gmail.com>
2023-09-13 14:55:10 +0300

    tests footer

--- 0x05-react_state/task_3/dashboard/src/Footer/Footer.test.js
+++ 0x05-react_state/task_3/dashboard/src/Footer/Footer.test.js
@@ -15,3 +15,57 @@ describe('Test for footer component', () => {
         );
     });
 });
+
+describe('Testing Footer Component context and state', () => {
+	beforeEach(() => {
+		StyleSheetTestUtils.suppressStyleInjection();
+	});
+
+	afterEach(() => {
+		jest.clearAllMocks();
+	});
+
+	it('Tests that there is no link rendered when user is logged out within context', () => {
+		const context = {
+			user: {
+				email: '',
+				password: '',
+				isLoggedIn: false
+			}
+		}
+
+		const wrapper = mount(
+			<UserContext.Provider value={context}>
+				<Footer />
+			</UserContext.Provider>
+		)
+
+		expect(wrapper.find('a').length).toBe(0);
+		expect(wrapper.find('a').exists()).toBe(false);
+		expect(wrapper.text()).not.toContain('Contact us');
+
+		wrapper.unmount();
+	})
+
+	it('Tests that there is a link rendered when user is logged in within context', () => {
+		const context = {
+			user: {
+				email: '',
+				password: '',
+				isLoggedIn: true
+			}
+		}
+
+		const wrapper = mount(
+			<UserContext.Provider value={context}>
+				<Footer />
+			</UserContext.Provider>
+		)
+
+		expect(wrapper.find('a').length).toBe(1);
+		expect(wrapper.find('a').exists()).toBe(true);
+		expect(wrapper.text()).toContain('Contact us');
+
+		wrapper.unmount();
+	})
+})
